075f14352c09daaad1cf72e908d810d64b76fbe5
Ryan Zwiefelhofer <ryan.zwiefelhofer@connexta.com>
2021-02-09 11:36:05 -0700

    Adds rest tests and default case for unknown content-types

--- src/recordsystem/rest.clj
+++ src/recordsystem/rest.clj
@@ -6,7 +6,9 @@
             [recordsystem.data :as data]
             [recordsystem.parse :as parse]
             [clojure.data.json :as json]
-            [clojure.pprint :refer [pprint]]))
+            [clojure.pprint :refer [pprint]]
+            [clojure.test :refer [deftest is]]
+            [clojure.string :as str]))
 
 
 (def sort->keyword
@@ -26,6 +28,8 @@
 
 
 (defn- query-records
+  "Queries for records and returns sorted by `sort`.
+  `dir` can be provided for direction, defaults `asc`"
   [sort dir]
   (if (not (contains? sort->keyword sort))
     (str "Could not find attribute: " sort)
@@ -39,9 +43,11 @@
   (condp = type
     "text/csv" parse/parse-comma
     "text/psv" parse/parse-pipe
-    "text/ssv" parse/parse-pipe))
+    "text/ssv" parse/parse-space
+    (fn [& args] (str "Could not find parser for: " type))))
 
 (defn- handle-post
+  "Parses `body` according to `type`"
   [body type]
   (let [parsed ((parse-fn type) body)]
     (doseq [item parsed] (data/store! item))
@@ -68,3 +74,31 @@
 
 (comment
   (ring/run-jetty #'wrapped-routes {:port 1337, :join? false}))
+
+
+(deftest
+  test-parse-fn
+  (is (= parse/parse-comma (parse-fn "text/csv")))
+  (is (= parse/parse-pipe (parse-fn "text/psv")))
+  (is (= parse/parse-space (parse-fn "text/ssv")))
+  (is (str/includes? ((parse-fn "notamimetype") nil)
+                     "notamimetype")))
+
+
+(deftest
+  test-query
+  (data/remove-all!)
+  (data/store! {:email "minnie@disney.com",
+                :last-name "mouse",
+                :color :pink,
+                :first-name "minnie",
+                :date-of-birth "11/18/1928"})
+  (data/store! {:email "mickey@disney.com",
+                :last-name "mouse",
+                :color :blue,
+                :first-name "mickey",
+                :date-of-birth "12/05/1901"})
+  (is (= '("minnie" "mickey") (map :first-name (query-records "birthdate" nil))))
+  (is (= '("mickey" "minnie") (map :first-name (query-records "birthdate" "desc"))))
+  (data/remove-all!))
+
